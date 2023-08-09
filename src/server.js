a0fcc7c092e7ea9077ea5ad83a53a2448f89e8dc
Rodrigo de Melo Rodrigues <rmelorodrigues@gmail.com>
2023-08-09 19:49:43 -0300

    adicionando regras

--- src/server.js
+++ src/server.js
@@ -14,15 +14,13 @@ app.use(
   })
 );
 
+app.use(express.json());
+
 const HOST = 'localhost'
 const PORT = 5000
 
-app.post('/teste', async (req, res) => {
-  console.log(req.body)
-})
 
-
-/* app.post('/usuario', async (req, res) => {
+app.post('/usuario', async (req, res) => {
   const { nome, idade, ativo, email } = req.body;
   const usuario = { nome, idade, ativo, email };
 
@@ -33,7 +31,7 @@ app.post('/teste', async (req, res) => {
     msg: "Usuário criado com sucesso!",
   });
 
-}) */
+})
 
 
 // endpoint inicial
@@ -61,7 +59,16 @@ app.put('/usuario/:id', async (req, res) => {
 app.get('/usuarios', async (req, res) => {
 
   const usuarios = await Usuario.find();
+
+  if(usuarios.length === 0) {
+
+  res.status(422).json({msg: 'Nenhum Usuario Encontrado!!!'});
+
+  } else {
+
   res.status(200).json(usuarios);
+
+  }
 
 });
 
@@ -71,9 +78,35 @@ app.get('/usuario/:id', async (req, res) => {
 
   const id = req.params.id;
 
+// Verifica se o Id passado na URL é válido, conforme o exemplo: 64caf89a27c5be42c8c6f540, o _id do mongo possui 24 caracteres, 
+// ou seja, só pode realizar a busca (linha 82), se o tamanho for válido.
+
+  if (id.length !== 24 ) {
+
+    res.status(422).json('Tamnho de ID inválido!!!');
+
+    return;
+
+  }
+
+  // Se o tamanho for válido, realiza a busca no banco de dados.
+
   const usuario = await Usuario.findOne({_id: id});
 
-  res.status(200).json(usuario);
+
+  // Tratamento, caso nehhum usuário com o id fornecido for encontrado.
+
+  if(usuario) {
+
+    res.status(200).json(usuario);
+
+  } else {
+
+    res.status(422).json('Usuário não encontrado!!!');
+
+  }
+
+  
 
 });
 
@@ -81,12 +114,34 @@ app.get('/usuario/:id', async (req, res) => {
 app.delete('/usuario/:id', async (req, res) => {
 
   const id = req.params.id;
+  console.log(id.length !== 24 );
 
-  const usuarioBD = await Usuario.findOne({_id: id});
+  if (id.length !== 24 ) {
 
-  await Usuario.deleteOne({_id: usuarioBD.id});
+    res.status(422).json('Tamnho de ID inválido!!!');
 
-  res.status(200).json("Usuário deletado com sucesso");
+    return;
+
+  }
+
+  // Se o tamanho for válido, realiza a busca no banco de dados.
+
+  const usuario = await Usuario.findOne({_id: id});
+
+
+  // Tratamento, caso nehhum usuário com o id fornecido for encontrado.
+
+  if(usuario) {
+
+    await Usuario.deleteOne({_id: usuario.id});
+  
+    res.status(200).json("Usuário deletado com sucesso");
+
+  } else {
+
+    res.status(422).json('Usuário não encontrado!!!');
+
+  }
 
 });
 
@@ -95,7 +150,7 @@ app.delete('/usuario/:id', async (req, res) => {
 const DB_USER = process.env.DB_USER
 const DB_PASS = process.env.DB_PASS
 
-console.log( DB_USER, DB_PASS)
+//console.log( DB_USER, DB_PASS)
 
 // conectando com banco Mongo Atlas - coloca senha <> e inclui o nome da tabela usuario
 mongoose.connect(
